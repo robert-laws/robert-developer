4f843b1c673e455012c0998eb58197148b20dbfc
Robert Laws <roblaws@me.com>
2019-06-19 21:44:40 +0300

    setup css and basic layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>About Me</h1>
     <p>Welcome to the website about things of interest.</p>
     <Link to="/">&larr; back to home</Link>
-  </>
+  </Layout>
 )
